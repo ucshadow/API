451a163fd23d465f90bf50d6ae1d7de31c041f3e
warleader <warleader@hotmail.co.uk>
2017-10-25 17:11:41 +0200

    dota2 API working example

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,16 +3,42 @@ import logo from '../logo.svg';
 import './App.css';
 
 class About extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {data: []}
+  }
+
+  componentDidMount = () => {
+    this.getAllData();
+  };
+
+  getAllData = () => {
+    let url = 'https://api.opendota.com/api/teams/39/matches';
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        this.setState({data: res});
+      });
+  };
+
+  showData = () => {
+    return this.state.data.map((e) => {
+      return(
+        <div>
+          {e.league_name}
+        </div>
+      )
+    })
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Welcome to about
-        </p>
+        {this.showData()}
       </div>
     );
   }
